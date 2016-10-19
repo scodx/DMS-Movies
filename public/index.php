4d0acf1c85a800e53df53e2b3b6090f8dfbf93bd
Oscar Sanchez <oscar.exe@gmail.com>
2016-10-18 21:20:31 -0400

    adding slim project, basic namespace, error handler

---
+++ public/index.php
@@ -0,0 +1,44 @@
+<?php
+if (PHP_SAPI == 'cli-server') {
+    // To help the built-in PHP dev server, check if the request was actually for
+    // something which should probably be served as a static file
+    $url  = parse_url($_SERVER['REQUEST_URI']);
+    $file = __DIR__ . $url['path'];
+    if (is_file($file)) {
+        return false;
+    }
+}
+
+require __DIR__ . '/../vendor/autoload.php';
+
+session_start();
+
+// Instantiate the app
+$settings = require __DIR__ . '/../src/settings.php';
+
+// Creating custom error handler
+$container = new \Slim\Container($settings);
+$container['errorHandler'] = function ($container) {
+    return function ($request, $response, $exception) use ($container) {
+        //Format of exception to return
+        $data = [
+            'message' => $exception->getMessage()
+        ];
+        return $container->get('response')->withStatus(500)
+            ->withHeader('Content-Type', 'application/json')
+            ->write(json_encode($data));
+    };
+};
+$app = new \Slim\App($container);
+
+// Set up dependencies
+require __DIR__ . '/../src/dependencies.php';
+
+// Register middleware
+require __DIR__ . '/../src/middleware.php';
+
+// Register routes
+require __DIR__ . '/../src/routes.php';
+
+// Run app
+$app->run();
---
+++ src/routes.php
@@ -0,0 +1,47 @@
+<?php
+// Routes
+
+$app->get('/[{name}]', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+
+
+
+
+/**
+ * Executes a benchmark in a given repository passed trough the url.
+ * Returns the results in JSON format.
+ */
+$app->get('/api/movies/', function ($request, $response, $args) {
+
+
+    /**
+     *  Parameters and other settings are stored in their
+     *  respective repositories. The idea was to be more
+     *  dynamic, passing the parameters in the request.
+     */
+    $repository         = $args["repository"];
+    $settingsRepository = $this->get('settings')["repositories"];
+
+    $benchmark = new \wbd\Benchmark($repository, $settingsRepository);
+    $benchmark->setMaxExecutionTime(60 *3); // Expanding max execution time to 3 minutes
+
+    $data = $benchmark->run(10);
+
+    return $response->withJson($data, 200);
+
+});
+
+
+
+
+
+
+
+
+
